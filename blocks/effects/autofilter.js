255595d497fddf3a660892e42c06ca8529afca1f
crisrdr <cristina.rodriguezdelosrios@estudiante.uam.es>
2026-04-16 13:54:16 +0200

    reorganización ficheros

--- blocks/effects/autofilter.js
+++ blocks/effects/autofilter.js
@@ -3,30 +3,34 @@ Blockly.Blocks['effect_autofilter'] = {
         this.appendDummyInput()
             .setAlign(Blockly.ALIGN_LEFT)
             .appendField("Filtro de Pasa Baja");
-            
+
         let freqField = new Blockly.FieldTextInput("4n");
         freqField.setTooltip(`Valores rápidos: 16n, 8n
 Valores lentos: 4n, 2n, 1m
 O números (ej: 5)`);
-        
+
         this.appendDummyInput()
             .setAlign(Blockly.ALIGN_RIGHT)
             .appendField("Frecuencia")
             .appendField(freqField, "FREQUENCY");
-            
+
         this.appendDummyInput()
             .setAlign(Blockly.ALIGN_RIGHT)
             .appendField("Profundidad")
-            .appendField(((function(f){ f.setTooltip(`Profundidad (0 a 1):
+            .appendField(((function (f) {
+                f.setTooltip(`Profundidad (0 a 1):
 0 = Efecto sutil
-1 = Efecto muy extremo`); return f; })(new Blockly.FieldNumber(1, 0, 1))), "DEPTH");
-            
+1 = Efecto muy extremo`); return f;
+            })(new Blockly.FieldNumber(1, 0, 1))), "DEPTH");
+
         this.appendDummyInput()
             .setAlign(Blockly.ALIGN_RIGHT)
             .appendField("Nivel de efecto (Wet)")
-            .appendField(((function(f){ f.setTooltip(`Nivel de efecto (0 a 1):
+            .appendField(((function (f) {
+                f.setTooltip(`Nivel de efecto (0 a 1):
 0 = Señal limpia original
-1 = Efecto al 100%`); return f; })(new Blockly.FieldNumber(1, 0, 1))), "WET");
+1 = Efecto al 100%`); return f;
+            })(new Blockly.FieldNumber(1, 0, 1))), "WET");
 
         this.appendStatementInput('STATEMENTS')
             .setCheck(null);
@@ -56,7 +60,7 @@ Blockly.JavaScript['effect_autofilter'] = function (block) {
 
     code += `const effect_${myNum} = new Tone.AutoFilter(${effectOptions}).connect(prev_dest_${myNum});\n`;
     code += `if (typeof effect_${myNum}.start === 'function') effect_${myNum}.start();\n`;
-    
+
     code += `current_dest = effect_${myNum};\n`;
 
     let innerCode = Blockly.JavaScript.statementToCode(block, 'STATEMENTS');
